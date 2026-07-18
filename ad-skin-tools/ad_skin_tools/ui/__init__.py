ae3d493657e7a985774a4a379ec49a88026724fb
Adien Dendra <47624392+AdienDendra@users.noreply.github.com>
2026-07-18 22:15:48 +1000

    Remove UI package import side effects

--- ad-skin-tools/ad_skin_tools/ui/__init__.py
+++ ad-skin-tools/ad_skin_tools/ui/__init__.py
@@ -1,11 +1,8 @@
-"""UI package for AD Skin Tools v4.2."""
+"""Maya UI modules for AD Skin Tools."""
 
-from ad_skin_tools.ui import joint_list
-from ad_skin_tools.ui import component_flood_section
-from ad_skin_tools.ui import tool_window
-
-
-component_flood_section.install(tool_window)
-
-
-__all__ = ["tool_window", "joint_list"]
+__all__ = [
+    "component_flood_section",
+    "joint_list",
+    "skin_operations",
+    "tool_window",
+]
