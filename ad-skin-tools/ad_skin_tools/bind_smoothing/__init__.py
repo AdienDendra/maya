eb55a8ffa8dc0a01de766744c08dc56d65925c76
Adien Dendra <47624392+AdienDendra@users.noreply.github.com>
2026-07-19 14:37:56 +1000

    Export v6.1 bind smoothing API

--- ad-skin-tools/ad_skin_tools/bind_smoothing/__init__.py
+++ ad-skin-tools/ad_skin_tools/bind_smoothing/__init__.py
@@ -4,8 +4,28 @@ from ad_skin_tools.bind_smoothing.diffusion import (
     BindDiffusionResult,
     diffuse_hard_ownership,
 )
+from ad_skin_tools.bind_smoothing.max_influences import (
+    MaxInfluenceProjectionResult,
+    enforce_maximum_influences,
+)
+from ad_skin_tools.bind_smoothing.options import BindSmoothingOptions
+from ad_skin_tools.bind_smoothing.solver import (
+    BindSmoothingResult,
+    solve_bind_smoothing,
+)
+from ad_skin_tools.bind_smoothing.validation import (
+    BindWeightValidationResult,
+    validate_bind_weights,
+)
 
 __all__ = [
     "BindDiffusionResult",
+    "BindSmoothingOptions",
+    "BindSmoothingResult",
+    "BindWeightValidationResult",
+    "MaxInfluenceProjectionResult",
     "diffuse_hard_ownership",
+    "enforce_maximum_influences",
+    "solve_bind_smoothing",
+    "validate_bind_weights",
 ]
