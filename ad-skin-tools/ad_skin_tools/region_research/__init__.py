d6244bd691581ed561379b6fd74086a06e968335
Adien Dendra <47624392+AdienDendra@users.noreply.github.com>
2026-07-22 11:12:29 +1000

    Export Region Research stage two API

--- ad-skin-tools/ad_skin_tools/region_research/__init__.py
+++ ad-skin-tools/ad_skin_tools/region_research/__init__.py
@@ -1,5 +1,11 @@
 """Independent Region Research package for AD Skin Tool."""
 
+from ad_skin_tools.region_research.boundary_contacts import (
+    BoundaryContactResearchResult,
+    BoundaryOwnerContact,
+    SecondaryRegionBoundary,
+    analyze_secondary_region_boundaries,
+)
 from ad_skin_tools.region_research.nearest_regions import (
     DEFAULT_DISTANCE_CHUNK_SIZE,
     NearestRegionResearchResult,
@@ -7,16 +13,30 @@ from ad_skin_tools.region_research.nearest_regions import (
 )
 from ad_skin_tools.region_research.runner import (
     get_last_result,
+    get_last_stage_01_result,
+    get_last_stage_02_result,
     print_stage_01_report,
+    print_stage_02_report,
     run_stage_01,
+    run_stage_02,
+    run_stage_02_from_stage_01,
 )
 
 
 __all__ = [
+    "BoundaryContactResearchResult",
+    "BoundaryOwnerContact",
     "DEFAULT_DISTANCE_CHUNK_SIZE",
     "NearestRegionResearchResult",
+    "SecondaryRegionBoundary",
+    "analyze_secondary_region_boundaries",
     "get_last_result",
+    "get_last_stage_01_result",
+    "get_last_stage_02_result",
     "print_stage_01_report",
+    "print_stage_02_report",
     "run_stage_01",
+    "run_stage_02",
+    "run_stage_02_from_stage_01",
     "solve_nearest_regions",
 ]
