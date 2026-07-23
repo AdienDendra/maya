cacfb2aaeb62fc069227fecb08dd73e03722b438
Adien Dendra <47624392+AdienDendra@users.noreply.github.com>
2026-07-23 20:27:52 +1000

    Use production Smooth and Region modules

--- ad-skin-tools/ad_skin_tools/launch.py
+++ ad-skin-tools/ad_skin_tools/launch.py
@@ -34,15 +34,14 @@ def reload_modules():
     import ad_skin_tools.components.selection as component_selection_weights
     import ad_skin_tools.components.flood as component_flood
     import ad_skin_tools.components.smooth as component_smooth
-    import ad_skin_tools.components.smooth_shared as component_smooth_shared
 
-    import ad_skin_tools.region_research.mesh_context as ownership_mesh_context
-    import ad_skin_tools.region_research.exact_distance_ties as ownership_exact_ties
-    import ad_skin_tools.region_research.closest_region_ownership as ownership_closest
-    import ad_skin_tools.region_research.secondary_surface_facing as ownership_facing
-    import ad_skin_tools.region_research.global_owner_assignment as ownership_global
-    import ad_skin_tools.region_research.closed_loop_ownership as ownership_loops
-    import ad_skin_tools.region_research.ownership_pipeline as ownership_pipeline
+    import ad_skin_tools.region.mesh_context as ownership_mesh_context
+    import ad_skin_tools.region.exact_distance_ties as ownership_exact_ties
+    import ad_skin_tools.region.closest_region_ownership as ownership_closest
+    import ad_skin_tools.region.secondary_surface_facing as ownership_facing
+    import ad_skin_tools.region.global_owner_assignment as ownership_global
+    import ad_skin_tools.region.closed_loop_ownership as ownership_loops
+    import ad_skin_tools.region.ownership_pipeline as ownership_pipeline
 
     import ad_skin_tools.bind_smoothing.diffusion as smoothing_diffusion
     import ad_skin_tools.bind_smoothing.cutoff_projection as smoothing_cutoff
@@ -88,7 +87,6 @@ def reload_modules():
         smoothing_options,
         smoothing_validation,
         smoothing_solver,
-        component_smooth_shared,
         smoothed_automatic_bind,
         automatic_surface_commands,
         add_influence,
@@ -102,8 +100,6 @@ def reload_modules():
     importlib.reload(smoothing_bind_section)
     importlib.reload(component_section)
     importlib.reload(tool_window)
-
-    component_section.smooth = component_smooth_shared
 
     _install_ui(
         tool_window,
@@ -119,7 +115,6 @@ def show(reload=False, auto_refresh=False):
     if reload:
         reload_modules()
 
-    from ad_skin_tools.components import smooth_shared as component_smooth_shared
     from ad_skin_tools.ui import (
         component_section,
         global_owner_tag,
@@ -128,8 +123,6 @@ def show(reload=False, auto_refresh=False):
         smoothing_bind_section,
         tool_window,
     )
-
-    component_section.smooth = component_smooth_shared
 
     _install_ui(
         tool_window,
